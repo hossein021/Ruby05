8195c721c6a154eb4f93ce5b395a3720eeff3773
Hossein <hosseinsalmani1373@gmail.com>
2021-06-04 12:24:00 +0430

    random,time,...

---
+++ main10.rb
@@ -0,0 +1,37 @@
+# rand , times , to_i , to_f ,...
+
+# a = "ali"
+# puts a
+
+# b = a * 10
+# puts b
+
+# ======================
+
+# 8.times{puts "Hossein"}
+
+# puts rand(10)
+# puts rand(100)
+# puts rand(1000)
+
+# ======================
+
+# 15.times{puts rand(200)}
+
+# ======================
+
+# s = "8".to_i
+# puts s
+
+# ======================
+
+# s = "8".to_f
+# puts s
+
+# ======================
+
+# s = "s".to_f
+# puts s
+
+# s = "s".to_i
+# puts s
